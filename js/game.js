78c9010921bbab14b827967c6878a39fdda093e7
Roman <romanbashtovyy@gmail.com>
2021-04-13 21:16:54 +0300

    setting canvas width

--- js/game.js
+++ js/game.js
@@ -40,21 +40,32 @@ let game = {
             realHeight: window.innerHeight
         };
 
+        if (data.realWidth/data.realHeight > data.maxWidth/data.maxHeight){
+            this.fitWidth(data);
+        } else {
+            this.fitHeight(data);
+        }
+        
+
         this.fitHeight(data);
 
         this.canvas.width = this.width;
         this.canvas.height = this.height;
         
     },
+    fitWidth(data){  
+        this.height = Math.round(this.width * data.realHeight / data.realWidth);
+        this.height = Math.min(this.height, data.maxHeight);
+        this.height = Math.max(this.height, data.minHeight);
+        this.width = Math.round(data.realWidth * this.height / data.realHeight);
+        this.canvas.style.width = '100%';
+    },
     fitHeight(data){
-        this.width = Math.floor(data.realWidth * data.maxHeight / data.realHeight);
+        this.width = Math.round(data.realWidth * data.maxHeight / data.realHeight);
         this.width = Math.min(this.width, data.maxWidth);
         this.width = Math.max(this.width, data.minWidth);
-
-        //this.height = data.maxHeight;
-        this.height = Math.floor(this.width * data.realHeight / data.realWidth);
+        this.height = Math.round(this.width * data.realHeight / data.realWidth);
         this.canvas.style.height = '100%';
-        console.log(this.width, this.height);
     },
     preload(callback) {
         let loaded = 0;
